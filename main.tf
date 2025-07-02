d34c9825d4786e20e411b9a63797ad714ab9a362
Naveen-DevOps-IT <snk29133@gmail.com>
2025-07-02 10:49:13 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+region = "us-east-1"
+}
+
+resource "aws_instance" "web" {
+count = 3
+ami = "ami-000ec6c25978d5999"
+instance_type = "t2.micro"
+tags = {
+Name = var.instance_name[count.index]
+}
+
+variable "instance_name" {
+  default = ["dev-server", "test-server", "prod-server"]
+}
